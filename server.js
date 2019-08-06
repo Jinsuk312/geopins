ced99f825c70b27c0b1c82278516b9f9c25c8fc6
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-06 12:57:11 -0500

    connected to mongodb atlas cluster

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose
+	.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+	.then(() => console.log('DB connected'))
+	.catch(err => console.error(err));
+
 const server = new ApolloServer({
 	typeDefs,
 	resolvers
