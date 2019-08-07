f1bbafae1a1a54f41de03fe8baf114b323720923
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-06 21:27:02 -0500

    google+ login button

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,19 +1,31 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import { withStyles } from '@material-ui/core/styles';
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+	const onSuccess = googleUser => {
+		const idToken = googleUser.getAuthResponse().id_token;
+		console.log({ idToken });
+	};
+
+	return (
+		<GoogleLogin
+			clientId="627105909732-uct4g7gut7167ppf8m56iiargmsf9c5t.apps.googleusercontent.com"
+			onSuccess={onSuccess}
+			isSignedIn={true}
+		/>
+	);
 };
 
 const styles = {
-  root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
-  }
+	root: {
+		height: '100vh',
+		display: 'flex',
+		justifyContent: 'center',
+		flexDirection: 'column',
+		alignItems: 'center'
+	}
 };
 
 export default withStyles(styles)(Login);
