9f57ac87299d8ea13a8bbd758b2fada61b5f26db
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-08 08:57:48 -0500

    authentication with apollo server

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require('apollo-server');
 // execute me resolver function
 const user = {
 	_id: '1',
@@ -5,8 +6,15 @@ const user = {
 	email: 'jinsukdev@gmail.com',
 	picture: 'https://cloudinary.com/asdf'
 };
+
+const authenticated = next => (root, args, ctx, info) => {
+	if (!ctx.currentUser) {
+		throw new AuthenticationError('You must be logged in');
+	}
+	return next();
+};
 module.exports = {
 	Query: {
-		me: () => user
+		me: authenticated((root, args, ctx) => ctx.currentUser)
 	}
 };
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const resolvers = require('./resolvers');
 
 require('dotenv').config();
 const mongoose = require('mongoose');
+const { findOrCreateUser } = require('./controllers/userController');
 
 mongoose
 	.connect(process.env.MONGO_URI, { useNewUrlParser: true })
@@ -13,7 +14,20 @@ mongoose
 
 const server = new ApolloServer({
 	typeDefs,
-	resolvers
+	resolvers,
+	context: async ({ req }) => {
+		let authToken = null;
+		let currentUser = null;
+		try {
+			authToken = req.headers.authorization;
+			if (authToken) {
+				currentUser = await findOrCreateUser(authToken);
+			}
+		} catch (err) {
+			console.error(`Unable to authenticate user with token ${authToken}`);
+		}
+		return { currentUser };
+	}
 });
 
 server.listen().then(({ url }) => {
