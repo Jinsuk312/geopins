ddb0e9cfacd813b10f6cd8450d51b67b5a5b9435
Justin Jinsuk Kim <jinsuk@justins-air.lan>
2019-08-08 11:48:04 -0500

    manage app state w/ usecontext/usereducer

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { GraphQLClient } from 'graphql-request';
 import { GoogleLogin } from 'react-google-login';
 import { withStyles } from '@material-ui/core/styles';
+import Context from '../../context';
+
 // import Typography from "@material-ui/core/Typography";
 
 const ME_QUERY = `{
@@ -14,13 +16,14 @@ const ME_QUERY = `{
 }`;
 
 const Login = ({ classes }) => {
+	const { dispatch } = useContext(Context);
 	const onSuccess = async googleUser => {
 		const idToken = googleUser.getAuthResponse().id_token;
 		const client = new GraphQLClient('http://localhost:4000/graphql', {
 			headers: { authorization: idToken }
 		});
 		const data = await client.request(ME_QUERY);
-		console.log({ data });
+		dispatch({ type: 'LOGIN_USER', payload: data.me });
 	};
 
 	return (
