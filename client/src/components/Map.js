dca228264f02e0547490c4947b9482be5687fa37
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-11 16:15:44 -0500

    displaying map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,42 +1,57 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react';
+import ReactMapGL from 'react-map-gl';
+import { withStyles } from '@material-ui/core/styles';
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
-
+const viewport = {
+	latitude: 37.7577,
+	longitude: -122.4376,
+	zoom: 13
+};
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+	return (
+		<div className={classes.root}>
+			<ReactMapGL
+				width="100vw"
+				height="calc(100vh - 64px)"
+				mapStyle="mapbox://styles/mapbox/streets-v9"
+				mapboxApiAccessToken="pk.eyJ1Ijoiamluc3VrbWlrIiwiYSI6ImNqdGR3a3JyeDFjMGg0M2w2Z3dpZno2dXcifQ.oZJB1kAkWUFczFPIuDmdPw"
+				{...viewport}
+			/>
+		</div>
+	);
 };
 
 const styles = {
-  root: {
-    display: "flex"
-  },
-  rootMobile: {
-    display: "flex",
-    flexDirection: "column-reverse"
-  },
-  navigationControl: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    margin: "1em"
-  },
-  deleteIcon: {
-    color: "red"
-  },
-  popupImage: {
-    padding: "0.4em",
-    height: 200,
-    width: 200,
-    objectFit: "cover"
-  },
-  popupTab: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column"
-  }
+	root: {
+		display: 'flex'
+	},
+	rootMobile: {
+		display: 'flex',
+		flexDirection: 'column-reverse'
+	},
+	navigationControl: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		margin: '1em'
+	},
+	deleteIcon: {
+		color: 'red'
+	},
+	popupImage: {
+		padding: '0.4em',
+		height: 200,
+		width: 200,
+		objectFit: 'cover'
+	},
+	popupTab: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		flexDirection: 'column'
+	}
 };
 
 export default withStyles(styles)(Map);
--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import withRoot from '../withRoot';
+import Map from '../components/Map';
 
 const App = () => {
-	return <Header />;
+	return (
+		<>
+			<Header /> <Map />
+		</>
+	);
 };
 
 export default withRoot(App);
