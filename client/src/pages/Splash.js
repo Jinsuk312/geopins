0a89f25367988ef044b6b06862da8194fc25b41b
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-11 15:59:57 -0500

    fixed authentication error

--- client/src/pages/Splash.js
+++ client/src/pages/Splash.js
@@ -3,7 +3,7 @@ import Context from '../context';
 import Login from '../components/Auth/Login';
 import { Redirect } from 'react-router-dom';
 const Splash = () => {
-	const { state } = useContext();
+	const { state } = useContext(Context);
 	return state.isAuth ? <Redirect to="/" /> : <Login />;
 };
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,19 @@
 const User = require('../models/User');
-
 const { OAuth2Client } = require('google-auth-library');
-const client = new OAuth2Client(process.env.OAuth2Client);
+const client = new OAuth2Client(process.env.OAUTH_CLIENT_ID);
 
 exports.findOrCreateUser = async token => {
-	//verify auth token
+	// verify auth token
 	const googleUser = await verifyAuthToken(token);
-	//check if the user exists
+	// check if the user exists
 	const user = await checkIfUserExists(googleUser.email);
-	// if user exists return them, otherwise create new user in db
+	// if user exists, return them; otherwise, create new user in db
 	return user ? user : createNewUser(googleUser);
 };
 
 const verifyAuthToken = async token => {
 	try {
-		const ticker = await client.verifyIdToken({
+		const ticket = await client.verifyIdToken({
 			idToken: token,
 			audience: process.env.OAUTH_CLIENT_ID
 		});
