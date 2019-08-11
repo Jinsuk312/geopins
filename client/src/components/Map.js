6c196870d0967bb4d10440722d72e98bf7373a2a
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2019-08-11 17:43:58 -0500

    added state so the viewport on the map can change

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactMapGL from 'react-map-gl';
 import { withStyles } from '@material-ui/core/styles';
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
-const viewport = {
+const INITIAL_VIEWPORT = {
 	latitude: 37.7577,
 	longitude: -122.4376,
 	zoom: 13
 };
 const Map = ({ classes }) => {
+	const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
 	return (
 		<div className={classes.root}>
 			<ReactMapGL
@@ -17,6 +18,7 @@ const Map = ({ classes }) => {
 				height="calc(100vh - 64px)"
 				mapStyle="mapbox://styles/mapbox/streets-v9"
 				mapboxApiAccessToken="pk.eyJ1Ijoiamluc3VrbWlrIiwiYSI6ImNqdGR3a3JyeDFjMGg0M2w2Z3dpZno2dXcifQ.oZJB1kAkWUFczFPIuDmdPw"
+				onViewportChange={newViewport => setViewport(newViewport)}
 				{...viewport}
 			/>
 		</div>
